60eac84365a0cd7a19a25d7764066d38be556537
MDC <mclesterdelacruz@yahoo.com>
2023-03-14 02:14:16 -0700

    tree builder

---
+++ 03-14-2023/app2.js
@@ -0,0 +1,10 @@
+function towerBuilder(nFloors) {
+    // build here
+    let space,star, tower = [];
+    for(i = 1; i <= nFloors; i++){
+      space = " ".repeat(nFloors - i);
+      star  = "*".repeat((2*i) - 1);
+      tower.push(`${space}${star}${space}`);
+    }
+    return tower
+  }
