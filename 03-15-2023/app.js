cdd9313134f84e7f2b74040474ef08788e1046cd
MDC <mclesterdelacruz@yahoo.com>
2023-03-15 02:10:04 -0700

    counting sheep

---
+++ 03-15-2023/app.js
@@ -0,0 +1,11 @@
+var countSheep = function (num){
+    let sheep_str = ""
+    if (num > 0){
+      for( let i = 1; i <= num; i++){
+        sheep_str += `${i} sheep...`
+    }}
+    else{
+      return sheep_str
+    }
+    return sheep_str
+  }
