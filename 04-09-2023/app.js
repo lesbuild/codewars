167ec3c4355f70f49993d9a6ef653396fc3f3a86
MDC <mclesterdelacruz@yahoo.com>
2023-04-09 00:47:20 -0700

    petal picking love meter

---
+++ 04-09-2023/app.js
@@ -0,0 +1,5 @@
+function howMuchILoveYou(nbPetals) {
+    let phrases = ["I love you", "a little", "a lot", "passionately", 
+                   "madly", "not at all"
+    ]
+    return phrases[(nbPetals -1) % phrases.length ]}
