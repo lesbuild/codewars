bf257124613deeb243de1208888a06f3c831b2cb
MDC <mclesterdelacruz@yahoo.com>
2023-05-06 02:35:49 -0700

    Digit * digit

---
+++ 05-06-2023/func2.js
@@ -0,0 +1,8 @@
+function squareDigits(num){
+    var array = num.toString().split('').map( function(int) {
+      var i = parseInt(int);
+      return i * i;
+    });
+    
+    return parseInt(array.join(""));
+  }
