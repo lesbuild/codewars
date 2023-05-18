77f9ca6bd39a3b68370f6d7ecd14e08a9768e1df
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2023-05-17 22:01:11 -0700

    Remove duplicates from list

---
+++ 05-17-2023/func2.js
@@ -0,0 +1,10 @@
+function distinct(a) {
+    let unique = [];
+    a.forEach(element => {
+        if (!unique.includes(element)) {
+            unique.push(element);
+        }
+    });
+    return unique;
+
+}
