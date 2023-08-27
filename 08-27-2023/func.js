3c76ab08a724fcc5aa402ef52c0d103e76e6114a
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2023-08-27 00:46:48 -0700

    Parts of a list

---
+++ 08-27-2023/func.js
@@ -0,0 +1,12 @@
+function partlist(arr) {
+    let newArray;
+    let returnArray=[];
+    for( var i=1; i<arr.length; i++){
+      newArray = [];
+      newArray.push(arr.slice(0,i).join(" "));
+      newArray.push(arr.slice(i).join(" "));
+      returnArray.push(newArray);
+    }
+    
+    return returnArray;
+}
