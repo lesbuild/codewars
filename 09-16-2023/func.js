856a86e8b23ea711d65b8abab03731ceb9a386db
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2023-09-17 00:44:10 -0700

    Maximum Product update

--- 09-16-2023/func.js
+++ 09-16-2023/func.js
@@ -1,12 +1,3 @@
-function adjacentElementsProduct(array) {
-  let maxProd = array[0] * array[1]
-  let temp = 0
-  for(let i = 0;i <array.length -1;i++){
-    temp = array[i] * array[i + 1]
-    if(temp > maxProd){
-      maxProd = temp
-    }
-    
-  }
-  return maxProd
+function incrementer(nums) { 
+  return nums.map((a,b)=>(a+b+1)%10)
 }
