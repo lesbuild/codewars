bea26837fd72447cb1bb5234df8855a0283f5c4b
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2023-11-02 00:06:00 -0700

    Create the rooms

---
+++ 11-02-2023/func.js
@@ -0,0 +1,12 @@
+class Room  {
+    constructor() {
+      this.name = '';
+      this.description = '';
+      this.completed= '';
+    }
+  }
+  var rooms = {
+    bedroom : new Room(),
+    kitchen : new Room(),
+    bathroom : new Room(),
+  }
