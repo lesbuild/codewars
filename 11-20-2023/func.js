eece460791461ae9621a995d1a477fc5ca1b3422
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2023-11-21 01:53:00 -0800

    Return String of First Characters

--- 11-20-2023/func.js
+++ 11-20-2023/func.js
@@ -1,4 +1,3 @@
 function first(arr, n) {
-    // return arr;
     return n == 0 ? [] : n == undefined ? [arr[0]] : arr.splice(0,n)
   }
---
+++ 11-21-2023/func.js
@@ -0,0 +1,3 @@
+function makeString(s){
+    return s.split(" ").map(a=>a.slice(0,1)).join("")
+  }
