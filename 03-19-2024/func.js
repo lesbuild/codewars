2ec395d71de042681205d43da6f1c26d45e3fd44
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2024-03-19 01:16:13 -0700

    Plus-minus-plus-plus-Count

---
+++ 03-19-2024/func.js
@@ -0,0 +1,9 @@
+function catchSignChange(arr) {
+    let count = 0
+    for(let i = 0;i<arr.length;i++){
+      if((arr[i] < 0 && arr[i + 1] >= 0) || (arr[i] >= 0 && arr[i + 1] < 0)) {
+        count += 1
+      }
+    }
+    return count
+  }
