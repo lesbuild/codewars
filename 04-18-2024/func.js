34079ee407fdb063feb5fe7b686363a6f92339ff
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2024-04-18 02:42:14 -0700

    Correct the string

---
+++ 04-18-2024/func.js
@@ -0,0 +1,9 @@
+function timeCorrect(timestring) {
+    if (!timestring)
+      return timestring
+      
+    if (!/^(\d\d:){2}\d\d$/.test(timestring))
+      return null
+      
+    return (new Date(0,0,0,...timestring.split(':'))).toString().slice(16,24)
+  }
