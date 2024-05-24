d0c5f60b07ad13dec676f5ba19c2bcaaf36f8e96
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2024-05-24 02:26:44 -0700

    Minimum Steps

---
+++ 05-24-2024/func.js
@@ -0,0 +1,9 @@
+function minimumSteps(numbers, value) {
+    const nums = numbers.slice().sort((a, b) => a - b);
+    for (let i = 0, sum = 0; i < nums.length; i++) {
+      sum += nums[i];
+      if (sum >= value) {
+        return i;
+      }
+    }
+  }
