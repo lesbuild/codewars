ef2991420d1dd65dc92664a53dd7e3e45b4e80b2
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2024-07-13 02:14:02 -0700

    Find all non-consecutive numbers

---
+++ 07-13-2024/func.js
@@ -0,0 +1,9 @@
+function allNonConsecutive (arr) {
+    let result = [];
+    arr.forEach((val, index) => {
+        if (val !== arr[index - 1] + 1 && index) {
+            result.push({i: index, n: val});
+        }
+    });
+    return result;
+}
