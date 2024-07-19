0171aa20eb3419734c481d92493f9727dc1c969b
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2024-07-19 00:19:28 -0700

    Wjere is the parent?

---
+++ 07-19-2024/func.js
@@ -0,0 +1,5 @@
+function findChildren(dancingBrigade){
+    return dancingBrigade.split("")
+                         .sort((a,b)=>a.localeCompare(b,"kf",{caseFirst:"upper"}))
+                         .join("");
+  }
