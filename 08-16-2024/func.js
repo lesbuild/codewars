2be7c81dadcbbddbddedee407f98b28d822b2da8
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2024-08-16 00:45:21 -0700

    Ghostbusters (whitespace removal)

---
+++ 08-16-2024/func.js
@@ -0,0 +1,4 @@
+function ghostBusters(building) {
+    return building.includes(" ") ? building.replaceAll(" ","") : "You just wanted my autograph didn't you?"
+                                    
+}
