087d7667410eb8743a221cd0288a9c410219be8f
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2024-09-14 03:00:45 -0700

    The first non repeated character in a string

---
+++ 09-14-2024/func.js
@@ -0,0 +1,8 @@
+function firstNonRepeated(s) {
+    for(var i = 0; i < s.length; i++) {
+      if (s.indexOf(s[i]) === s.lastIndexOf(s[i])) {
+        return s[i]
+      }
+    }
+    return null
+  }
