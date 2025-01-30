0dd53a2ae57628ebce6e467e802e990360e45bed
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-01-30 01:48:49 -0800

    Basic number object and its properties

--- 1-30-2025/func.js
+++ 1-30-2025/func.js
@@ -5,6 +5,6 @@ function whatNumberIsIt(n){
       case (n==Number.MIN_VALUE): return "Input number is Number.MIN_VALUE";
       case (n==Number.NEGATIVE_INFINITY): return "Input number is Number.NEGATIVE_INFINITY";
       case (isNaN(n)): return "Input number is Number.NaN";
-      default: return `Input number is  + ${n}
+      default: return `Input number is  + ${n}`;
     }
   }
