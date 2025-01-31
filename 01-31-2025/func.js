2b3f2da8493ff88c3900786ab298f8fb476123e2
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-01-31 02:24:23 -0800

    update sentence

--- 01-31-2025/func.js
+++ 01-31-2025/func.js
@@ -1,4 +1,4 @@
 function goGoGadget(){
     let gadgets = ["Copter","Binoculars","Brella","Coat","Ears","Hands","Magnets","Magnifying Glass","Mallet","Skates","Skis","Springs","Watch"]
-    return `Go go gadget ${gadgets[Math.floor(Math.random() * gadgets.length)]}`
+    return `Go go gadget ${gadgets[Math.floor(Math.random() * gadgets.length)]}!`
 }
