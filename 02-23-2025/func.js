68a097c9f031d730badb559dbb12c7208c9d8123
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-02-23 22:41:01 -0800

    Find your Villain Name from Birthday

---
+++ 02-23-2025/func.js
@@ -0,0 +1,7 @@
+function getVillainName(birthday){
+    const m = ["Evil","Vile","Cruel","Trashy","Despicable","Embarrassing","Disreputable","Atrocious","Twirling","Orange","Terrifying","Awkward"];
+    const d = ["Mustache","Pickle","Hood Ornament","Raisin","Recycling Bin","Potato","Tomato","House Cat","Teaspoon","Laundry Basket"];
+    let first = m[birthday.getMonth()]
+    let last = d[birthday.getDate() % 10]
+    return `The ${first} ${last}`
+  }
