2d29847b6a5d32311d65f8f19691d6713f1ab262
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-03-08 02:17:45 -0800

    alignment for readability

--- 03-08-2025/func.js
+++ 03-08-2025/func.js
@@ -1,11 +1,10 @@
 function targetSave(intitial,interest,target){
-    function targetSave(intitial,interest,target){
-        let money = intitial
-        interest = interest / 100
-        let years = 0
-        while(money < target){
-            money *= (1 + interest)
-            years++
-        }
-        return years
+    let money = intitial
+    interest = interest / 100
+    let years = 0
+    while(money < target){
+        money *= (1 + interest)
+        years++
     }
+    return years
+}
