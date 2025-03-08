4d4255e9f4d3673d606d2da526d9e69c73f1b658
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-03-08 02:13:50 -0800

    how many years to save this much

---
+++ 03-08-2025/func.js
@@ -0,0 +1,11 @@
+function targetSave(intitial,interest,target){
+    function targetSave(intitial,interest,target){
+        let money = intitial
+        interest = interest / 100
+        let years = 0
+        while(money < target){
+            money *= (1 + interest)
+            years++
+        }
+        return years
+    }
