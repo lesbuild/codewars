bb8bd9836363bb990d1582e6471bc52566aa57d9
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-03-27 00:48:57 -0700

    Relax day

---
+++ 03-27-2025/func.js
@@ -0,0 +1,3 @@
+function funTimes(spareTime){
+    return spareTime <= 30 ? "Go for a quick food" : spareTime <= 60 ? "Get food and read some books" : "Go to the gym for an hour and do some chores"
+}
