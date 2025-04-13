8156766cbc430a670f6efaf011eb111cf0b58461
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-04-13 01:06:01 -0700

    Updated to better version

--- 04-13-2025/func.js
+++ 04-13-2025/func.js
@@ -1,3 +1,7 @@
 function isItLetter(character) {
   return "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".includes(character)
 }
+
+function isItLetter(character) {
+  return /[a-zA-Z]/g.test(character)
+}
