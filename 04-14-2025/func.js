a3dc63511dd10d7899a40bd2d6c122cf1d24a7b4
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-04-14 02:43:43 -0700

    Object Drills: Quarks

--- 04-14-2025/func.js
+++ 04-14-2025/func.js
@@ -2,7 +2,7 @@ class Quark {
     constructor(color, flavor) {
       this.color = color
       this.flavor = flavor
-      this.baryon_number = 1 / 3
+      this.baryon_number = 1/3
     }
     interact(other) {
       [this.color, other.color] = [other.color, this.color]
