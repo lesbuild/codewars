8b4bae12ede3dd6b13fd77bdaeb8b632f99e4b86
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-04-22 00:59:10 -0700

    update with description

--- 04-22-2025/func.js
+++ 04-22-2025/func.js
@@ -10,3 +10,23 @@ function catMouse(x, j){
       return "boring without all three"
     }
   }
+//   Description:
+//   You will be given a string (x) featuring a cat 'C', a dog 'D' and a mouse 'm'. The rest of the string will be made up of '.'.
+  
+//   You need to find out if the cat can catch the mouse from its current position. The cat can jump at most (j) characters, and cannot jump over the dog.
+  
+//   So:
+  
+//   if j = 5:
+  
+//   ..C.....m...D returns 'Caught!' <-- not more than j characters between the cat and the mouse
+  
+//   .....C............m......D returns 'Escaped!' <-- as there are more than j characters between the two, the cat cannot jump far enough
+  
+//   if j = 10:
+  
+//   ...m.........C...D returns 'Caught!' <-- Cat can jump far enough and jump is not over dog
+  
+//   ...m....D....C....... returns 'Protected!' <-- Cat can jump far enough, but dog is in the way, protecting the mouse
+  
+//   Finally, if not all three animals are present, return 'boring without all three'
