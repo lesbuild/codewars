0868bf9472128e446b1572550a97a331d82a10ac
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-05-07 00:40:17 -0700

    tidying code

--- 05-07-2025/func.js
+++ 05-07-2025/func.js
@@ -1,8 +1,8 @@
 function arrAdder(arr) {
     var sentence = ""
-    for(var i = 0; i < arr[0].length; i++){
-      for(var j = 0; j < arr.length; j++){
-        sentence+=arr[j][i]
+    for (var i = 0; i < arr[0].length; i++){
+      for (var j = 0; j < arr.length; j++){
+        sentence += arr[j][i]
       }
       sentence += " "
     }
