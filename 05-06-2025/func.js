c3e9850e59c613f1143cf5778994c6c477115e43
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-05-06 23:16:00 -0700

    Refactor

--- 05-06-2025/func.js
+++ 05-06-2025/func.js
@@ -1,3 +1,6 @@
 function arr2bin(arr){
     return arr.every(a=>typeof a === "number" && a % 1 === 0) ? arr.reduce((a,b)=>a+b,0).toString(2) : false
   }
+  function arr2bin(arr){
+    return arr.every(a=>Number.isInteger(a)) ? arr.reduce((a,b)=>a+b,0).toString(2) : false
+  }
