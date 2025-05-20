c776c9531dfee166e5d2b9b348fe8acd466b522c
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-05-20 01:23:48 -0700

    Pizza payment

---
+++ 05-20-2025/func.js
@@ -0,0 +1,8 @@
+function michaelPays(costs) {
+  return costs < 5 ? +costs.toFixed(2) : costs / 3 > 10 ? +(costs - 10).toFixed(2) : +(costs - (costs / 3)).toFixed(2)
+}
+// Kate and Michael want to buy a pizza and share it. Depending on the price of the pizza, they are going to divide the costs:
+
+// If the pizza is less than €5,- Michael invites Kate, so Michael pays the full price.
+// Otherwise Kate will contribute 1/3 of the price, but no more than €10 (she's broke :-) and Michael pays the rest.
+// How much is Michael going to pay? Calculate the amount with two decimals, if necessary.
