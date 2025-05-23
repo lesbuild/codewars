8e051de4568fdb8c1ec4014ecb29741a62498f13
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-05-23 01:28:53 -0700

    updated

--- 05-23-2025/func.js
+++ 05-23-2025/func.js
@@ -1,4 +1,5 @@
 function battleOfChar(str){
-    return str.match(/[a-z]/gi).length > str.match(/[0-9]/g).length ?
-           "string wins!" : "number wins!"
+    return str === "" ? "empty string" : str.match(/[a-z]/gi).length > str.match(/[0-9]/g).length ?
+           "string wins!" : str.match(/[a-z]/gi).length > str.match(/[0-9]/g).length ?
+           "number wins!" : "Draw, an even match!"
 }
