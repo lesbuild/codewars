39e257d1fa2dec416ab2bf6fc33b5edf85449e37
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-06-16 00:25:13 -0700

    updated with description

--- 06-16-2025/func.js
+++ 06-16-2025/func.js
@@ -1,3 +1,8 @@
 function arraySum(arr) {
   return arr.flat(Infinity).filter(item => !isNaN(parseFloat(item)) && isFinite(item)).reduce((a,b)=>a+b)
 }
+// You are given an array of values.
+
+// Sum every number value in the array, and any nested arrays (to any depth).
+
+// Ignore all other types of values.
