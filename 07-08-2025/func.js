63b4955e4e7c9cdea91f44abff3660eb95c135a1
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-07-08 02:38:09 -0700

    updated with instructions

--- 07-08-2025/func.js
+++ 07-08-2025/func.js
@@ -2,3 +2,7 @@ function arrowPointing(str){
     return str.includes(">") && str.includes("<") ? `The -> arrow is pointing at index ${str.indexOf(">") + 1} and <- arrow is pointing at ${str.indexOf("<") - 1}` :
             str.includes(">") ? `The -> arrow is pointing at index ${str.indexOf(">") + 1}` :  `<- arrow is pointing at ${str.indexOf("<") - 1}`
 }
+// Show where the arrow['s] is pointing
+// Arrow is always pointing somewhere
+// Example:
+// " <--------> " would return The -> arrow is pointing at index 11 and <- arrow is pointing at 0
