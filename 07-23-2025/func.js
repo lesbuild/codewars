39f71cbce0586ea1071c0346e33334e7e93fedb2
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-07-23 23:31:36 -0700

    Where we going?

---
+++ 07-23-2025/func.js
@@ -0,0 +1,6 @@
+function wayOfMajority(str){
+    let arr = str.split("")
+    let left = arr.filter(a=>a=="<").length
+    let right = arr.filter(b=>b==">").length
+    return left > right ? "We are going left!" : right > left ? "We are going right!" : "Let's stay here and decide again!"
+}
