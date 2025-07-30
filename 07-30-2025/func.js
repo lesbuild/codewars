a997ccb156707b809cb39751fb6421f690e7e5ea
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-07-30 01:31:28 -0700

    throwback comedy lyric

---
+++ 07-30-2025/func.js
@@ -0,0 +1,6 @@
+function applePen(){
+     let first = "I have"
+     let thing1 = "apple"
+     let thing2 = "pen"
+     return `${first} an ${thing1}, ${first} a ${thing2} uhm ${thing1 + thing2}!`
+}
