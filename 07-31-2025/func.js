488bf2580b40d5e8e732220f38f690cf2c18af9c
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-07-31 17:16:06 -0700

    updated

--- 07-31-2025/func.js
+++ 07-31-2025/func.js
@@ -1,5 +1,5 @@
 function fusion(str1,idx, str2){
-    return str.substring(0,idx) + str2 + str1.substring(idx)
+    return str1.substring(0,idx) + str2 + str1.substring(idx)
 }
 // fuse 2 strings
 // Insert str2 in str1 using index provided
