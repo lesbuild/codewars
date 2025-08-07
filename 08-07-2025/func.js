f0cf6aa170ece0d96cbf19fdaa687cc8a74e22c3
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-08-07 01:30:11 -0700

    updated instructions

--- 08-07-2025/func.js
+++ 08-07-2025/func.js
@@ -1,3 +1,4 @@
 function placeToHome(str){
     return str.substring(str.indexOf("o"),str.indexOf("x")).length
 }
+// distance from start to finish with "o" being start and "x" will be the end line 
