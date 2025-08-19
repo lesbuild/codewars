30831369d3ec938924daecb6e76a25d3d1f71e77
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-08-19 03:07:39 -0700

    Updated code

--- 08-19-2025/func.js
+++ 08-19-2025/func.js
@@ -1,7 +1,7 @@
 function battleOfSides(num){
     num = num.toString();
-    let left = num.substring(0,num.length/2).split("").reduce((a,b)=>parseInt(a)+parseInt(b),0)
-    let right = num.substring(num.length/2).split("").reduce((a,b)=>parseInt(a)+parseInt(b),0)
+    let left = num.substring(0,Math.ceil(num.length/2)).split("").reduce((a,b)=>parseInt(a)+parseInt(b),0)
+    let right = num.substring(Math.floor(num.length/2)).split("").reduce((a,b)=>parseInt(a)+parseInt(b),0)
     return left > right ? "Left side wins!" : left < right ? "Right side wins!" : "Draw"
 }
 // given number check if by adding first half, which is the left side, and second half, which is the right side, that the total is greater than the other.
