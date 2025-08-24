dc420a3c172c1d9b9d366c591a6ea9aa56554266
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-08-24 02:50:25 -0700

    updated with instruction

--- 08-24-2025/func.js
+++ 08-24-2025/func.js
@@ -2,3 +2,4 @@ function directionSignal(str){
     return str.match(/>/g) ? `Going right at index ${str.indexOf(">")}` : 
                              `Going left at index ${str.indexOf("<")}`
 }
+// Find out which direction the line is going and find the index of it
