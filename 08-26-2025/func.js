c7d31425752815270327d8e588e209301ea6cd7e
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-08-26 01:33:05 -0700

    Complete The Pattern #6 - Odd Ladder

---
+++ 08-26-2025/func.js
@@ -0,0 +1,29 @@
+function pattern(n){
+  let output=[];
+  for(let i = 1;i<=n;i+=2){
+    output.push(`${i}`.repeat(i))
+  }
+  return output.join("\n");
+}
+// Task:
+
+// You have to write a function pattern which creates the following pattern (see examples) up to the desired number of rows.
+
+// If the Argument is 0 or a Negative Integer then it should return "" i.e. empty string.
+
+// If any even number is passed as argument then the pattern should last upto the largest odd number which is smaller than the passed even number.
+
+// ###Examples:
+
+// pattern(9):
+
+// 1
+// 333
+// 55555
+// 7777777
+// 999999999
+// pattern(6):
+
+// 1
+// 333
+// 55555
