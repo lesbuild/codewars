a6eef54156ee8e811ba7cf23deeb63fc03c95df3
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-08-28 22:42:25 -0700

    Sum it power it

--- 08-28-2025/func.js
+++ 08-28-2025/func.js
@@ -1,3 +1,4 @@
-function partial(str){
-    return
+function targetPower(arr, target){
+    return Math.pow(arr.reduce((a,b)=>a+b)-target, target)
 }
+// Sum the array but exclude the target then raise the power to the target
