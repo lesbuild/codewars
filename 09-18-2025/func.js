f1a26704ed626c148d66246e2aaaca3bc7155454
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-09-18 01:16:07 -0700

    Sum it continuously

---
+++ 09-18-2025/func.js
@@ -0,0 +1,10 @@
+function add(arr) {
+  return arr.map((e,i)=>e+arr.slice(0,i).reduce((a,b)=>+a+b,0))
+}
+// Description:
+// Make a function "add" that will be able to sum elements of list continuously and return a new list of sums.
+
+// For example:
+
+// add [1,2,3,4,5] == [1, 3, 6, 10, 15], because it's calculated like 
+// this : [1, 1 + 2, 1 + 2 + 3, 1 + 2 + 3 + 4, 1 + 2 + 3 + 4 + 5]
