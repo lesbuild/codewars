6473ffe2779d4f470fd8fd1273cb427ec05fcb53
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-10-06 03:13:07 -0700

    Added regular expression solution

--- 10-06-2025/func.js
+++ 10-06-2025/func.js
@@ -7,6 +7,10 @@ function doubleCheck(str){
     }
     return false
 }
+
+function doubleCheck(str){
+    return /(.)\1/i.test(str)
+}
 // Your job is to build a function which determines whether or not there are double characters in a string (including whitespace characters). For example aa, !! or   .
 
 // You want the function to return true if the string contains double characters and false if not. The test should not be case sensitive; for example both aa & aA return true.
