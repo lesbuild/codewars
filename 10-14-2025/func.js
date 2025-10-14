6e725c9b8eb38969ebed624301d80d74c6cb6dfd
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-10-14 01:07:43 -0700

    Add Opposite

---
+++ 10-14-2025/func.js
@@ -0,0 +1,14 @@
+function oppositeAttract(arr) {
+    return arr.map((e,i)=>e+(arr.length-1-i)) 
+}
+Description:
+
+// Add an element opposite of their index
+
+// 1st element add last index number, 2nd element add second to last index number and so forth...
+
+// Every element is an integer.
+
+// Example:
+
+// oppositeAttract([1,1,1,1]) => [4,3,2,1]
