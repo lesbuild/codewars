01058ab6cb0507b9f5bcc76e922c0291ac37ffea
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-11-16 23:32:01 -0800

    Test Your Knowledge Of Function Scope

---
+++ 11-16-2025/func.js
@@ -0,0 +1,11 @@
+function add(a) {
+  return function(b) {
+    return a + b
+  }
+}
+// Write a function that adds from two invocations.
+
+// All inputs will be integers.
+
+// add(3)(4)  // 7
+// add(12)(20) // 32
