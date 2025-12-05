dac16d6aa0fd75632b09c4f2143f3139f9c5c126
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-12-05 02:53:40 -0800

    "x" marks the spot

---
+++ 12-05-2025/func.js
@@ -0,0 +1,50 @@
+function xMarksTheSpot(matrix) {
+  let loc = []
+  for(let i = 0;i<matrix.length;i++){
+    for(let j = 0;j<matrix[i].length;j++){
+      if(matrix[i][j] == "x"){
+        loc.push(i,j)
+      }
+    }
+  }
+  return loc.length > 2 ? [] : loc
+}
+// Task:
+// Given a two dimensional array, return the co-ordinates of x.
+
+// If x is not inside the array, or if x appears multiple times, return [].
+
+// The co-ordinates should be zero indexed in row-major order.
+// You should assume you will always get an array as input. The array will only contain 'x's and 'o's.
+
+// Examples
+// Input: []
+// Return an empty array if input is an empty array => []
+
+// Input: [
+//   ['o', 'o'],
+//   ['o', 'o']
+// ]
+// Return an empty array if no x found => []
+
+// Input: [
+//   ['x', 'o'],
+//   ['o', 'x']
+// ]
+// Return an empty array if more than one x found => []
+
+// Input: [
+//   ['x', 'o'],
+//   ['o', 'o']
+// ]
+// Return [0,0] when x at top left => [0, 0]
+
+// Input: [
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o'],
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'x', 'o'],
+//   ['o', 'o', 'o', 'o', 'o', 'o', 'o', 'o']
+// ]
+// Return [4,6] for the example above => [4, 6]
