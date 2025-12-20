aa4f9f82573b4cb3c636140a560832acb63fad16
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2025-12-20 02:22:00 -0800

    Friday the 13th Part 1

---
+++ 12-20-2025/func.js
@@ -0,0 +1,28 @@
+function killcount(counselors, jason){
+  let killed = []
+  for(let i = 0;i<counselors.length;i++){
+    if(counselors[i][1] < jason){
+      killed.push(counselors[i][0])
+    }
+  }
+  return killed
+}
+// It's Friday the 13th, and Jason is ready for his first killing spree!
+
+// Create a function, killcount, that accepts two arguments: an array of array pairs (the conselor's name and intelligence - ["Chad", 2]) and an integer representing Jason's intellegence.
+
+// Ruby, Python, Crystal:
+
+// counselors = [["Chad", 2], ["Tommy", 9]]
+// jason = 7
+// JavaScript:
+
+// let counselors = [["Chad", 2], ["Tommy", 9]];
+// let jason = 7;
+// PHP:
+
+// $counselors = [["Chad", 2], ["Tommy", 9]];
+// $jason = 7;
+// Your function must return an array of the names of all the counselors who can be outsmarted and killed by Jason.
+
+// Happy Friday the 13th!
