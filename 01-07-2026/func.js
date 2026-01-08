981b06a2d0d24ea9d479cc219fe15a8d5cb1d65c
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2026-01-07 22:34:12 -0800

    Regexp Basics - is it all whitespace?

---
+++ 01-07-2026/func.js
@@ -0,0 +1,4 @@
+String.prototype.whitespace=function(){
+  return /^\s*$/.test(this);
+}
+// Implement String#whitespace?(str) (Ruby), String.prototype.whitespace(str) (JavaScript), String::whitespace(str) (CoffeeScript), or whitespace(str) (Python), which should return true/True if given object consists exclusively of zero or more whitespace characters, false/False otherwise.
