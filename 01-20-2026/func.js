bf96b9a01ec615589178591d9c9c1a54a3379554
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2026-01-20 02:03:20 -0800

    Inverting a Hash

---
+++ 01-20-2026/func.js
@@ -0,0 +1,38 @@
+function invertHash(hash) {
+  let invert = {}
+  for (const [key, value] of Object.entries(hash)){
+    invert[value] = key
+  }
+  return invert
+}
+// Description:
+// Summary
+// Given a Hash made up of keys and values, invert the hash by swapping them.
+
+// Examples
+// Given:
+
+//   { a: '1',
+//     b: '2',
+//     c: '3' }
+
+// Return:
+
+//   { 1: 'a',
+//     2: 'b',
+//     3: 'c' }
+
+
+
+// Given:
+
+//   { foo:   'bar',
+//     hello: 'world' }
+
+// Return:
+
+//   { bar:   'foo',
+//     world: 'hello' }
+// Notes
+// Keys and values may be of any type appropriate for use as a key.
+// All hashes provided as input will have unique values, so the inversion is involutive. In other words, do not worry about identical values stored under different keys.
