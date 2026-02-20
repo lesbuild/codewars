06cfbf8820f30b5bb451d6201807c66a4932b46b
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2026-02-19 23:22:55 -0800

    International Morse Code Encryption

---
+++ 02-18-2026/02-19-2026/func.js
@@ -0,0 +1,15 @@
+function encryption(message) {
+  //YourCodeHere
+  //CHAR_TO_MORSE preloaded to convert characters into Morse code
+  return message.split(" ").map(a=>a.split("").map(b=>CHAR_TO_MORSE[b]).join(" ")).join("   ")
+}
+// Description:
+// Write a function that will encrypt a given sentence into International Morse Code, both the input and out puts will be strings.
+
+// Characters should be separated by a single space. Words should be separated by a triple space.
+
+// For example, "HELLO WORLD" should return -> ".... . .-.. .-.. --- .-- --- .-. .-.. -.."
+
+// To find out more about Morse Code follow this link: https://en.wikipedia.org/wiki/Morse_code
+
+// A preloaded object/dictionary/hash called CHAR_TO_MORSE will be provided to help convert characters to Morse Code.
