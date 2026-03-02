d24df770d59c6e2497ce55efdad26e0e0914363b
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2026-03-02 01:03:46 -0800

    updated error

--- 03-01-2026/func.js
+++ 03-01-2026/func.js
@@ -1,3 +1,3 @@
 function goMid(arr1,arr2){
-    return arr1.join(arr2).sort((a,b)=>a - b)[arr1.join(arr2).length/2]
+    return arr1.concat(arr2).sort((a,b)=>a - b)[Math.floor(arr1.concat(arr2).length/2)]
 }
