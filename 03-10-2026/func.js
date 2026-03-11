99d8d4ef82fe650a0f24e98823951d54454840d0
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2026-03-10 22:52:52 -0700

    Mobile Display Keystrokes

---
+++ 03-10-2026/func.js
@@ -0,0 +1,40 @@
+function mobileKeyboard(str){
+  let one = "1234567890*#"
+  let two = "adgjmptw"
+  let three = "behknqux"
+  let four = "cfilorvy"
+  let five = "sz"
+  let count = 0
+  for(let i = 0 ;i<str.length;i++){
+    if(one.includes(str[i])){
+      count += 1
+    }
+    else if(two.includes(str[i])){
+      count += 2
+    }
+    else if(three.includes(str[i])){
+      count += 3
+    }
+    else if(four.includes(str[i])){
+      count += 4
+    }
+    else if(five.includes(str[i])){
+      count += 5
+    }
+  }
+  return count
+}
+// Description:
+// Do you remember the old mobile display keyboards? Do you also remember how inconvenient it was to write on it?
+// Well, here you have to calculate how many keystrokes you have to do for a specific word.
+
+// This is the layout:
+
+
+// Given a string, return the number of keystrokes necessary to type it. You can assume that the input will entirely consist of characters included in the mobile layout (lowercase letters, digits, and the symbols * and #).
+
+// Examples
+// "*#"       =>  2 (1 + 1)
+// "123"      =>  3 (1 + 1 + 1)
+// "abc"      =>  9 (2 + 3 + 4)
+// "codewars" => 26 (4 + 4 + 2 + 3 + 2 + 2 + 4 + 5)
