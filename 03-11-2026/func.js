4bfbb8bf697ebdb0498c57b17b881b9730af453d
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2026-03-11 22:57:21 -0700

    Frisk the whole grocery store

---
+++ 03-11-2026/func.js
@@ -0,0 +1,9 @@
+function groceryRun(arr){
+    return arr.reduce((a,b)=>a+b)
+}
+// Description:
+
+// What's the average speed to go through the whole aisle of the store
+
+
+// groceryRun([1,2,3]) => 2
