a413b0c94bb6aa0f6180a52f1590a236e0d4135d
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2026-03-27 01:04:59 -0700

    heggeleggleggo

---
+++ 03-27-2026/func.js
@@ -0,0 +1,15 @@
+function heggeleggleggo(word){
+  let conso = "bcdfghjklmnpqrstvwxyz"
+  return word.split("").map(a=>conso.includes(a.toLowerCase()) ? a + "egg" : a).join("")
+}
+// Description:
+// Egg Talk.
+
+// Insert an egg after each consonant. If there are no consonants, there will be no eggs. Argument will consist of a string with only alphabetic characters and possibly some spaces.
+
+// Example
+// hello => heggeleggleggo
+
+// eggs => egegggeggsegg
+
+// FUN KATA => FeggUNegg KeggATeggA
