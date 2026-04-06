2c4106ef0d967a308179c56a4fe146a24f9d9561
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2026-04-05 23:27:35 -0700

    simplified hunt

--- 04-05-2026/func.js
+++ 04-05-2026/func.js
@@ -1,11 +1,10 @@
 function easterHunt(str) {
-    const regex = /\((\d+)\)/g;
-    const matches = [...str.matchAll(regex)];
-    return matches.map(m => m[1]).reduce((a,b)=>Number(a)+Number(b));
+    const eggFound = [...str.matchAll(/\((\d+)\)/g)];
+    return eggFound.map(m => m[1]).reduce((a,b)=>Number(a)+Number(b));
 }
 
 // Description:
 
-// Find all the number inside an egg "(num)" and sum it all up.
+// Find all the number inside an egg "(number)" and sum it all up.
 
 // easter("(23)asd12(2)")=>25
