31ae295aa3dd3029ca737a61368ff696e771f1db
Mclester Dela Cruz <mclesterdelacruz@yahoo.com>
2026-06-09 23:49:30 -0700

    Simple sort

---
+++ 06-09-2026/func.js
@@ -0,0 +1,7 @@
+function sortArray(arr) {
+  return arr.sort((a, b) => a - b);
+}
+
+// Description:
+
+// Given an array of integers, sort the array in ascending order and return the sorted array. If the array is empty, return an empty array.
